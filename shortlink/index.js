c05153eca6770506a4538f934bf0cd41c7a15aaf
Jakob de Guzman <55227471+Proximitynow19@users.noreply.github.com>
2022-12-14 09:54:43 +1300

    Remove conflicting shorts + favicon

--- shortlink/index.js
+++ shortlink/index.js
@@ -17,7 +17,7 @@ app.post("/create", (req, res) => {
   if (!req.body || !req.body.href || !req.body.otp)
     return res.status(400).json({
       status: 404,
-      data: "Missing body, 'href', 'otp' property.",
+      data: "Missing body, 'href' or 'otp' property.",
       success: false,
     });
 
@@ -34,6 +34,11 @@ app.post("/create", (req, res) => {
 
   let id = req.body.id || nanoid(6);
 
+  if (shorts.find((k) => k.id == id))
+    return res
+      .status(409)
+      .json({ status: 409, data: `ID ${id} already exists.`, success: false });
+
   shorts.push({ id, href: req.body.href });
 
   res.status(201).json({ status: 201, data: id, success: true });
@@ -43,6 +48,45 @@ app.post("/create", (req, res) => {
 
 app.get("/", (_, res) => {
   res.sendFile(join(__dirname, "index.html"));
+});
+
+app.delete("/:id", (req, res) => {
+  if (!req.body || !req.body.otp)
+    return res.status(400).json({
+      status: 404,
+      data: "Missing body or 'otp' property.",
+      success: false,
+    });
+
+  if (
+    !speakeasy.totp.verify({
+      secret: secretFile.ascii,
+      encoding: "ascii",
+      token: req.body.otp,
+    })
+  )
+    return res
+      .status(403)
+      .json({ status: 403, data: "Incorrect OTP.", success: false });
+
+  const short = shorts.find((k) => k.id == req.params.id);
+
+  if (!short)
+    return res.status(404).json({
+      status: 404,
+      data: `Short '${req.params.id}' not found.`,
+      success: false,
+    });
+
+  shorts = shorts.filter((k) => k.id != req.params.id);
+
+  res.status(204).json({
+    status: 204,
+    data: `Deleted short ${req.params.id}.`,
+    success: true,
+  });
+
+  writeFileSync("db.json", JSON.stringify({ shorts }), "utf-8");
 });
 
 app.get("/:id", (req, res) => {
