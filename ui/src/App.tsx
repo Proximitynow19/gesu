3aa54013002e6eb036083812c739ef066a35e166
Jakob de Guzman <55227471+Proximitynow19@users.noreply.github.com>
2022-12-16 17:14:57 +1300

    Added transition between pages

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,6 +1,9 @@
 import type { Component } from "solid-js";
 
-import { Routes, Route } from "@solidjs/router";
+import { Routes, Route, useIsRouting } from "@solidjs/router";
+
+import { Motion, Presence } from "@motionone/solid";
+import { Rerun } from "@solid-primitives/keyed";
 
 import styles from "./App.module.css";
 import Navbar from "./Navbar";
@@ -12,14 +15,25 @@ import Community from "./pages/Community";
 const App: Component = () => {
   return (
     <main class={styles.App}>
-      <Navbar />
-      <section class={styles.Page}>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/library" element={<Library />}></Route>
-          <Route path="/community" element={<Community />}></Route>
-        </Routes>
-      </section>
+      <Navbar />{" "}
+      <Presence exitBeforeEnter>
+        <Rerun on={useIsRouting()}>
+          <Motion
+            initial={{ opacity: 0, x: 50 }}
+            animate={{ opacity: 1, x: 0, transition: { delay: 0.05 } }}
+            transition={{ duration: 0.1 }}
+            exit={{ opacity: 0, x: -50 }}
+          >
+            <section class={styles.Page}>
+              <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/library" element={<Library />}></Route>
+                <Route path="/community" element={<Community />}></Route>
+              </Routes>
+            </section>
+          </Motion>
+        </Rerun>
+      </Presence>
       <div class={styles.Player}>
         <Player />
       </div>
